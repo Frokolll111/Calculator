2d216bcd77f12914316ac698cca920703f963360
Frokolll111 <buther1433@gmail.com>
2023-06-14 17:02:26 +0700

    Сделал домашние задание

--- src/main/java/pro/sky/calculator1/calculatorService.java
+++ src/main/java/pro/sky/calculator1/calculatorService.java
@@ -6,8 +6,4 @@ public interface calculatorService {
     String minusCalculator(int num1, int num2);
     String multiplyCalculator(int num1, int num2);
     String divideCalculator(int num1, int num2);
-
-    int divideByZero(int num1, int num2);
-
-    String noDataSubmitted(String num1, String num2);
 }
--- src/main/java/pro/sky/calculator1/calculatorServiceImpl.java
+++ src/main/java/pro/sky/calculator1/calculatorServiceImpl.java
@@ -1,45 +1,43 @@
 package pro.sky.calculator1;
 
 import org.springframework.stereotype.Service;
+
 @Service
 public class calculatorServiceImpl implements calculatorService {
     public String helloCalculator() {
         return "<b>Добро пожаловать в калькулятор</b>";
     }
-
+@Override
     public String plusCalculator(int num1, int num2) {
+        if (num1 == 0 || num2 == 0) {
+            return "Ошибка нету 1 или 2 числа ";
+        }
         int sum = num1 + num2;
         return  num1 + " + "+ num2 +" = " + sum;
     }
-
+    @Override
     public String minusCalculator(int num1, int num2) {
+        if (num1 == 0 || num2 == 0) {
+            return "Ошибка нету 1 или 2 числа ";
+        }
         int sum = num1 - num2;
         return  num1 + " - "+ num2 +" = " + sum;
     }
-
+    @Override
     public String multiplyCalculator(int num1, int num2) {
+        if (num1 == 0 || num2 == 0) {
+            return "Ошибка нету 1 или 2 числа ";
+        }
         int sum = num1 * num2;
         return  num1 + " * "+ num2 +" = " + sum;
     }
 
-
+    @Override
     public String divideCalculator(int num1, int num2) {
+        if (num2 == 0) {
+            return " Делить на ноль нельзя " ;
+        }
         int sum = num1 / num2;
         return  num1 + " / "+ num2 +" = " + sum;
-    }
-
-    public int divideByZero( int num1, int num2) {
-        if (num2 != 0) {
-            return num1 / num2;
-        } else {
-            throw new RuntimeException();
-        }
-    }
-
-    public String noDataSubmitted(String num1, String num2) {
-        if (num1 != null || num2 != null ) {
-            return null;
-        }else
-            throw new RuntimeException();
     }
 }
